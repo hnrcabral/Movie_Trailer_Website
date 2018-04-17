77aad6b7e9cb7a9ac6198cb9be9e1d172b9333a5
Henry Cabral <jrcbr19@gmail.com>
2018-04-17 19:32:37 -0400

    Added comments and reformatted to PEP 8

--- media.py
+++ media.py
@@ -1,11 +1,12 @@
-import webbrowser
+import webbrowser  # import webbrowser to open websites
 
 
-class Movie():
+class Movie():  # class Movie serves as a template for each instance within
+                # entertainmentcenter.py file
     '''This class provides a way to store movie related information'''
 
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+                 trailer_youtube):  # outlines the variables to be used
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
